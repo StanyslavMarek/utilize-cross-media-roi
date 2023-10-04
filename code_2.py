f5823845c43af86490805b691b669287e20c77ce
StanyslavMarek <140631407+StanyslavMarek@users.noreply.github.com>
2023-10-04 23:46:11 +0200

    Create code_2.py

---
+++ code_2.py
@@ -0,0 +1,59 @@
+    for item in data:
+    data = generate_random_data()
+def generate_random_data():
+    main()
+if __name__ == "__main__":
+
+
+import random
+    data = [random.randint(1, 100) for _ in range(10)]
+    return data
+
+        print(f"Random Number: {item}")
+
+def main():
+function generateRandomData() {
+    }
+    const data = generateRandomData();
+        data.push(Math.floor(Math.random() * 100) + 1);
+    for (let i = 0; i < 10; i++) {
+        console.log("Random Number: " + item);
+    return data;
+    const data = [];
+    data.forEach(item => {
+    });
+
+
+}
+main();
+}
+function main() {
+
+    data = generate_random_data()
+def generate_random_data():
+import random
+    return data
+    main()
+    for item in data:
+
+def main():
+        print(f"Random Number: {item}")
+
+    data = [random.randint(1, 100) for _ in range(10)]
+if __name__ == "__main__":}
+        console.log("Random Number: " + item);
+    const data = generateRandomData();
+
+    }
+function main() {
+    });
+    for (let i = 0; i < 10; i++) {
+
+function generateRandomData() {
+main();
+    data.forEach(item => {
+        data.push(Math.floor(Math.random() * 100) + 1);
+}
+
+    const data = [];
+    return data;
